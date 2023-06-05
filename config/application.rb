7910454885cbb836986c5093357b2263afbd9cfb
Alan R. Fachini <hi@alfakini.com>
2023-06-04 22:21:51 -0300

    Undo custom loading

--- config/application.rb
+++ config/application.rb
@@ -1,28 +1,6 @@
 require_relative "boot"
 
-# require "rails/all"
-require "rails"
-
-unloaded = %w(
-  action_mailer/railtie
-  action_mailbox/engine
-  action_text/engine
-  active_job/railtie
-  action_cable/engine
-)
-
-%w(
-  active_storage/engine
-  active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-  rails/test_unit/railtie
-).each do |railtie|
-  begin
-    require railtie
-  rescue LoadError
-  end
-end
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
