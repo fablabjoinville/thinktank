4ebeae2e7c5c39936e79dab7589e37f7f0f3b47e
Alan R. Fachini <hi@alfakini.com>
2023-09-22 17:01:05 -0300

    Update cancancan configs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,24 +17,31 @@ class Ability
       can :manage, :all
     when :admin
       can :manage, :all
+
       cannot [:create, :update, :destroy], User, authorization_level: [:super_admin, :admin]
     when :secretary
       can :read, :all
+
+      can :manage, Company
+
+      can :manage, Member
+
       can :manage, Person, authorization_level: [:person]
       can :read, Person, authorization_level: [:facilitator, :secretary, :admin, :super_admin]
-      can :manage, Member
     when :facilitator
-      can :read, Cluster
-      can :update, Cluster, person_id: user.id
+      can :read, Cluster, person_id: user.id
 
-      can :read, Team
-      can :update, Team, clusters: { person_id: user.id }
+      can :manage, Event, team: { clusters: { person_id: user.id }}
+      can :create, Event
 
-      can :read, Event
-      can :update, Event, team: { clusters: { person_id: user.id }}
 
-      can :read, Member
-      can :update, Member, team: { clusters: { person_id: user.id }}
+      can :update, Person, id: user.id
+      can :read, Person, id: user.id
+
+      can :read, Team, clusters: { person_id: user.id }
+
+      can :read, User, id: user.id
+      can :update, User, id: user.id
     end
   end
 end
